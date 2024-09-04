1a51c5e11d8c4556dcdd390ecda118ccdcb1905d
Everton Alves <alveseverton023@gmail.com>
2024-09-04 12:27:15 -0300

    comentarios na matriz

--- matrizautocompleta.cpp
+++ matrizautocompleta.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define LIN 10
 #define COL 10
 
-int main ()
+int main()
 
 {
     int x;
@@ -17,16 +17,19 @@ int main ()
     cout << "\n   Tabela de multiplicacao\n";
     cout << setw(6) << 0;
 
+    // gera a linha superior dos multiplicadores
     for (x = 1; x < COL; x += 1)
         cout << setw(3) << x;
     cout << endl;
 
     cout << "   ";
 
+    // linha de serparação da tabela
     for (x = 0; x < 3 * COL; x += 1)
         cout << "-";
     cout << endl;
 
+    // gera tabela de multiplicação
     for (y = 0; y < LIN; y += 1)
     {
         cout << setw(2) << y << "|";
