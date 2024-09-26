05507a06756934fdcbc7b0bf3c8fecd5282efa66
Everton Alves <alveseverton023@gmail.com>
2024-09-26 17:18:56 -0300

    terminararray

--- output/Exercicios/funcaovetor.cpp
+++ output/Exercicios/funcaovetor.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
+int mostra[vetor] = {vetorv};
+int levetor (int vetort, int vetorv)
+{
+    cout << "Digite o tamanho do vetor"<<endl;
+    cin >> vetort;
+
+    for(int i = 0; i < vetort; i++)
+    {
+        
+        cout << "Digite o valor do vetor:"<<endl;
+        cin >> vetorv;
+    }
+
+}
+
+
+int main ()
+{
+    int vet, vev;
+    int mostra[levetor()] = {vev};
+}
+
+//exemplo vetores
+//int main()
+//{
+//    int notas[5] = {10, 20, 5, 55, 100};
+
+ //   cout << "Notas dos alunos: " << endl;
+ //   for (int i = 0; i < 5; i++)
+ //   {
+ //       cout << "Nota " << (i + 1) << ": " << notas[i] << endl;
+ //   }
+ //   return 0;
+//}
