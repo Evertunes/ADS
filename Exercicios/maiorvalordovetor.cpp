ce8bf60abb87a040768ef62652b6f60d5795a718
Everton Alves <alveseverton023@gmail.com>
2024-09-28 19:14:53 -0300

    finalizado vetor

--- Exercicios/maiorvalordovetor.cpp
+++ Exercicios/maiorvalordovetor.cpp
@@ -1,57 +1,51 @@
 #include <iostream>
 using namespace std;
 
-bool armazenarNumeros(int numeros[], int quantidade, int vetor[])
+int armazenarNumeros(int numeros[], int quantidade)
 {
 
-    for (int i = 0; i <= quantidade; ++i)
-    {
-        //Recebe os números
+    for (int i = 0; i < quantidade; ++i)
+    {   
+        // Recebe os números
         cout << "Digite o numero: " << (i + 1) << ": " << endl;
         cin >> numeros[i];
-
-        for (int i = 0; i <= quantidade; i++)
-        {
-            cin >> vetor[i];
-        }
-
-        // Encontrar o maior valor
-        int maior = vetor[0];
-        for (int i = 0; i < quantidade; i++)
-        {
-            if (vetor[i] > maior)
-            {
-                maior = vetor[i];
-            }
-        }
-
-        //Exibe os números armazenados
+    }
+        // Exibe os números armazenados
         cout << "=====================" << endl;
         cout << "Numeros armazenados: ";
         for (int i = 0; i < quantidade; ++i)
         {
             cout << numeros[i] << " ";
         }
+        
         cout << endl;
 
+        // Encontrar o maior valor
+        int maior = numeros[0];
+        
+        for (int i = 0; i < quantidade; i++)
+        {
+            if (numeros[i] > maior)
+            {
+                maior = numeros[i];
+            }
+        }
         // Exibir o maior valor
         cout << "===================================" << endl;
         cout << "O maior valor do vetor e: " << maior << endl;
-    }
-    return 0;
+    
+    return 1;
 }
 
 int main()
 {
 
-    //Recebe o tamnho do vetor
+    // Recebe o tamnho do vetor
     int quantidade;
     cout << "=======================================" << endl;
     cout << "Quantos numeros voce deseja armazenar? " << endl;
     cin >> quantidade;
-
-    int vetor[quantidade];
-
+    
     // Declarando o vetor com tamanho fixo
     // Criamos um limite de números a serem armazenados
     int numeros[100]; // Supondo um máximo de 100 números
@@ -65,8 +59,8 @@ int main()
     }
     else
     {
-        //Chama a função principal
-        armazenarNumeros(numeros, quantidade, vetor);
+        // Chama a função principal
+        armazenarNumeros(numeros, quantidade);
     }
     return 0;
 }
